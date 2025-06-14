ceb6a99b7f42d21c66be48baf93fa17a9d453b8a
Ganesh Gundu <ganesh.g.cse@gmail.com>
2025-06-14 10:32:12 +0000

    observe generated content

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import os
 
+import sys, json, logging
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        json_log_object = {
+            "severity": record.levelname,
+            "message": record.getMessage(),
+        }
+        json_log_object.update(getattr(record, "json_fields", {}))
+        return json.dumps(json_log_object)
+logger = logging.getLogger(__name__)
+sh = logging.StreamHandler(sys.stdout)
+sh.setFormatter(JsonFormatter())
+logger.addHandler(sh)
+logger.setLevel(logging.DEBUG)
+
+
 from flask import Flask, request
 
 import google.auth
@@ -17,6 +34,8 @@ def hello_world():
     animal = request.args.get("animal", "dog") 
     prompt = f"Give me 10 fun facts about {animal}. Return this as html without backticks."
     response = model.generate_content(prompt)
+    json_fields = {"prompt": prompt, "response": response.to_dict()}
+    logger.debug("Content is generated", extra={"json_fields": json_fields})
     return response.text
 
 if __name__ == "__main__":
